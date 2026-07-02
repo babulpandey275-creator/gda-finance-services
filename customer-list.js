30eda9bc932103220626f8db678dd8a20b1f1d14
babulpandey275-creator <babulpandey275@gmail.com>
2026-07-03 03:10:45 +0530

    Create customer-list.js

---
+++ customer-list.js
@@ -0,0 +1,50 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const customerList = document.getElementById("customerList");
+
+async function loadCustomers() {
+
+  customerList.innerHTML = "Loading...";
+
+  const snapshot = await getDocs(collection(db, "customers"));
+
+  customerList.innerHTML = "";
+
+  snapshot.forEach((doc) => {
+
+    const c = doc.data();
+
+    customerList.innerHTML += `
+
+    <div class="card">
+
+      <h3>${c.name}</h3>
+
+      <p><b>Mobile:</b> ${c.mobile}</p>
+
+      <p><b>Address:</b> ${c.address}</p>
+
+      <p><b>Loan:</b> ₹${c.loan}</p>
+
+      <p><b>Daily EMI:</b> ₹${c.emi}</p>
+
+      <p><b>Remaining:</b> ₹${c.remainingAmount}</p>
+
+      <a class="btn" href="collection.html?id=${doc.id}">
+      Daily Collection
+      </a>
+
+    </div>
+
+    `;
+
+  });
+
+}
+
+loadCustomers();
