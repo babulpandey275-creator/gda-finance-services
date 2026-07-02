3946b9678b9f0c304813a0b38a22f060ca36626b
babulpandey275-creator <babulpandey275@gmail.com>
2026-07-03 03:13:08 +0530

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,52 @@
+import { db } from "./firebase.js";
+
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  addDoc,
+  collection
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const ref = doc(db, "customers", id);
+const snap = await getDoc(ref);
+
+const c = snap.data();
+
+document.getElementById("name").textContent = c.name;
+document.getElementById("loan").textContent = c.loan;
+document.getElementById("emi").textContent = c.emi;
+document.getElementById("remaining").textContent = c.remainingAmount;
+
+document.getElementById("saveBtn").onclick = async () => {
+
+  const amount = Number(document.getElementById("amount").value);
+
+  if (!amount || amount <= 0) {
+    alert("Enter valid amount");
+    return;
+  }
+
+  const remaining = c.remainingAmount - amount;
+  const paidDays = c.paidDays + Math.floor(amount / c.emi);
+
+  await updateDoc(ref, {
+    remainingAmount: remaining,
+    paidDays: paidDays
+  });
+
+  await addDoc(collection(db, "collections"), {
+    customerId: id,
+    customerName: c.name,
+    amount: amount,
+    date: new Date()
+  });
+
+  alert("Collection Saved Successfully");
+
+  location.reload();
+
+};
