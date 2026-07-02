e671d7dc6191b26c7ade1492e325bc5a2be44b01
babulpandey275-creator <babulpandey275@gmail.com>
2026-07-03 00:16:33 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFG2At-wHGTiUg6cc2kjiKmgbK-dSgXKw",
+  authDomain: "gda-finance-services.firebaseapp.com",
+  projectId: "gda-finance-services",
+  storageBucket: "gda-finance-services.firebasestorage.app",
+  messagingSenderId: "146207390486",
+  appId: "1:146207390486:web:076e980ea1404c665b6e37",
+  measurementId: "G-92D8JNBHBF"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("Firebase Connected Successfully");
+
+export { db };
