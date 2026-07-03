ef8d84a359eb06c7b8b79ffd64fbe3fd14bbeb1f
babulpandey275-creator <babulpandey275@gmail.com>
2026-07-03 19:41:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import { db } from "./firebase.js";
-
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 window.addEventListener("load", () => {
     console.log("GDA Finance Services Loaded");
     console.log(db);
@@ -13,6 +16,31 @@ function showMessage(page) {
     else {
         alert(page + " Coming Soon");
     }
+}async function loadDashboard() {
+
+  if (!document.getElementById("totalCustomers")) return;
+
+  const snapshot = await getDocs(collection(db, "customers"));
+
+  let totalCustomers = snapshot.size;
+  let totalLoan = 0;
+  let totalCollection = 0;
+  let totalRemaining = 0;
+
+  snapshot.forEach(doc => {
+    const c = doc.data();
+
+    totalLoan += Number(c.loan || 0);
+    totalCollection += Number(c.totalCollected || 0);
+    totalRemaining += Number(c.remainingAmount || 0);
+  });
+
+  document.getElementById("totalCustomers").textContent = totalCustomers;
+  document.getElementById("totalLoan").textContent = totalLoan;
+  document.getElementById("totalCollection").textContent = totalCollection;
+  document.getElementById("totalRemaining").textContent = totalRemaining;
 }
+
+loadDashboard();
 
 window.showMessage = showMessage;
